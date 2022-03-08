001f80d7de9bc65418ec243c298ada09bae6b6b1
flowscode <joshuadawkins95@gmail.com>
2022-03-08 19:24:34 +0000

    renamed folder

---
+++ Training/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  # region = "us-east-1"
+  # access_key = var.aws_ak
+  # secret_key = var.aws_sk
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-0c293f3f676ec4f90"
+  instance_type = var.instance_type
+  count         = 2
+
+  tags = {
+    # Name = "MyServer-(${var.test_dev[count.index]})"
+    Name = "MyServer-${local.environments[count.index]}"
+  }
+}
---
+++ Training/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_ak" {
+  type = string
+}
+variable "aws_sk" {
+  type = string
+}
+
+variable "test_dev" {
+  type = list(string)
+}
+
+variable "instance_type" {
+  type = string
+}
