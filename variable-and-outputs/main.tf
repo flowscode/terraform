d8bae387761bebc3c5e55eda675649ded4801441
flowscode <joshuadawkins95@gmail.com>
2022-03-14 17:16:28 +0000

    commented out the outputs

--- variable-and-outputs/main.tf
+++ variable-and-outputs/main.tf
@@ -34,9 +34,9 @@ resource "aws_s3_bucket" "bucket" {
   depends_on = [aws_instance.my_server]
 }
 
-output "public_ip_0" {
-  value = aws_instance.my_server[0].public_ip
-}
-output "public_ip_1" {
-  value = aws_instance.my_server[1].public_ip
-}
+# output "public_ip_0" {
+#   value = aws_instance.my_server[0].public_ip
+# }
+# output "public_ip_1" {
+#   value = aws_instance.my_server[1].public_ip
+# }
